feb17924eff752dd9e68741e58baa741d9702a00
Langbien <pierre.langbien@gmail.com>
2023-03-10 12:41:32 +0100

    mise en place test player join a game

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,11 @@ import { GameMachine, GameModel } from './machine/GameMachine'
 import { interpret } from 'xstate'
 
 const machine = interpret(GameMachine).start()
+/*
 console.log(machine.send(GameModel.events.join('1', '1')).changed)
 console.log(machine.send(GameModel.events.join('1', '1')).changed)
 console.log(machine.send(GameModel.events.join('2', '2')).changed)
-
+*/
 /*
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
